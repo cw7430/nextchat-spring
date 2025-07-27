5afea534838068fd98b00fce5096bd78424089b3
cw7430 <cw282@naver.com>
2025-07-28 01:12:27 +0900

    validation

--- src/main/java/com/next/chat/dto/request/auth/NativeSignInRequestDto.java
+++ src/main/java/com/next/chat/dto/request/auth/NativeSignInRequestDto.java
@@ -1,10 +1,13 @@
 package com.next.chat.dto.request.auth;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class NativeSignInRequestDto {
+    @NotBlank(message = "아이디와 비밀번호를 입력해주세요.")
     private String userId;
+    @NotBlank(message = "아이디와 비밀번호를 입력해주세요.")
     private String password;
     private boolean isAuto;
 }
